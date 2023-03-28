2fd4269b2e2114199918f7445990095378150cbd
Fanttatraieste <118627923+Fanttatraieste@users.noreply.github.com>
2023-03-28 15:28:54 +0300

    some css

--- js/gallery.js
+++ js/gallery.js
@@ -66,9 +66,63 @@ const gallery = [
 ];
 
 const galleryList = document.querySelector(".gallery__list");
-console.log("check");
+
+//load items
+window.addEventListener("DOMContentLoaded", function () {
+  displayGallery(gallery);
+});
+
+//variabila aceasta va fi constant lista de imagini, care isi va schimba ordinea elementelor la fiecare scroll
+let galleryDynamic = gallery; //initializare
+
+//this function will change the order of the elements
+function changeOrder(list = [], order) {
+  //list - list of elements
+  //order - type of order (1 for down, -1 for up)
+  let newList = [];
+  let n = list.length;
+  if (order === 1) {
+    for (let i = 0; i < list.length - 1; i++) newList.push(list[i + 1]);
+    newList.push(list[0]);
+    return newList;
+  } else if (order === -1) {
+    newList.push(list[n - 1]);
+    for (let i = 0; i < n - 1; i++) newList.push(list[i]);
+    return newList;
+  }
+}
 
 galleryList.addEventListener("scroll", function () {
-  let n = Math.random();
-  console.log(n);
+  let scroll = galleryList.scrollTop;
+
+  if (scroll == 4) {
+    console.log(scroll);
+
+    galleryDynamic = changeOrder(galleryDynamic, 1);
+    displayGallery(galleryDynamic);
+    //galleryList.scrollTop = 0;
+  } else if (scroll == 0) {
+    console.log(scroll);
+
+    galleryDynamic = changeOrder(galleryDynamic, -1);
+    displayGallery(galleryDynamic);
+    //galleryList.scrollTop = 4;
+  }
 });
+
+//display iimages based on the order
+function displayGallery(items = []) {
+  let displayGalleryList = items.map(function (item) {
+    const { srcset, src, sizes, alt } = item;
+    return ` <li><img 
+                class="gallery__image" 
+                srcset= "${srcset}"
+                src="${src}"
+                sizes="${sizes}"
+                alt="${alt}"
+                ></li>
+              `;
+  });
+  displayGalleryList = displayGalleryList.join("");
+  galleryList.innerHTML = displayGalleryList;
+}
